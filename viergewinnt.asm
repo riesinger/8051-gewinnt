0a48c6c67031eca52feccdde9b4363b2193a4a06
riesinger <mail@pascal-riesinger.de>
2018-11-27 15:08:30 +0100

    Implement input logic

--- viergewinnt.asm
+++ viergewinnt.asm
@@ -2,8 +2,12 @@ COLS EQU P0
 ROWS EQU P1
 KEYPAD EQU P2
 
-SPIELER1 EQU 020h
-SPIELER2 EQU 028h
+AKTIVERSPIELER EQU 18h
+; Eingabebereit wenn 20.0 = 1
+EINGABEBEREIT EQU 00h
+
+SPIELER1 EQU 038h
+SPIELER2 EQU 040h
 SPIELER2AKTIV EQU 030h
 
 cseg at 0h
@@ -17,19 +21,25 @@ reti
 org 20h
 ; Init setzt Ausgänge und Speicherbereiche auf Initialwerte, aktiviert timer
 init:
-MOV P0, 000h
-MOV P1, 000h
+MOV COLS, 000h
+MOV ROWS, 000h
 mov IE, #10010010b
 mov tmod, #00000010b
-mov R6, #00h
 mov R7, #00h
 mov tl0, #0c0h
 mov th0, #0c0h
 setb tr0
+mov aktiverspieler, #spieler1
 call display
+setb EINGABEBEREIT
 clr C
 
 loop:
+; Eingabe abfragen
+call eingabe_abfragen
+; Spielstand aktualisieren
+
+; Spieler wechseln
 
 JMP loop
 
@@ -104,3 +114,53 @@ MOV COLS, A
 SETB ROWS.7
 CLR ROWS.7
 RET
+
+eingabe_abfragen:
+; Wenn wir nicht eingabebereit sind, dann gehen wir wieder zurück
+jnb eingabebereit, eingabe_fertig
+; Lesen, welcher Button auf dem Keypad gedrückt wurde
+; R3 = Eingabe
+mov a, p2
+cpl a
+mov r3, a
+; TODO: Gegebenenfalls überprüfen, ob mehrere Buttons gedrückt wurden
+; R2 = Schleifenzähler
+mov r2, #07h
+
+vergleich:
+
+mov a, #SPIELER1
+add a, r2
+mov r0, a
+mov a, @R0
+anl a, r3
+mov r1, a
+
+mov a, #SPIELER2
+add a, r2
+mov r0, a
+mov a, @R0
+anl a, r3
+
+orl a, r1
+; Wir können einfügen
+jz einfuegen
+; Schleifenabbruch
+mov a, r2
+jz eingabe_fertig
+
+dec r2
+jmp vergleich
+
+einfuegen:
+mov a, AKTIVERSPIELER
+add a, r2
+mov r0, a
+mov a, @r0
+orl a, r3
+mov @r0, a
+
+eingabe_fertig:
+ret
+
+end
